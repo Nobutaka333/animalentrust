8ec1de460aeb1cdca99e49dc6a0c66db04fbe86e
Nobutaka333 <taka9861+techcamp@gmail.com>
2021-04-13 17:43:29 +0900

    詳細、編集、削除機能の実装

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,6 @@
 class AnimalsController < ApplicationController
+  before_action :set_animal, only: [:edit, :show, :update, :destroy]
+
   def index
     @animals = Animal.includes(:user)
   end
@@ -16,8 +18,32 @@ class AnimalsController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @animal.update(animal_params)
+      redirect_to animal_path(@animal.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @animal.destroy
+    redirect_to root_path
+  end
+
   private
   def animal_params
-    params.require(:animal).permit(:name, :text, :category_id,:prefecture_id,:storage_place_pet_id,:storage_time_id,:image).merge(user_id: current_user.id)
+    params.require(:animal).permit(:name, :text, :category_id,:prefecture_id,:animal_area,:storage_place_pet_id,:storage_time_id,:image).merge(user_id: current_user.id)
   end
+end
+
+def set_animal
+  @animal = Animal.find(params[:id])
 end
